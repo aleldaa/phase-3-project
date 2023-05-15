4de635944d8ef2799c3859014d3e2202d2fe5d61
Luna Daniels <aleldaproductions@gmail.com>
2023-05-15 14:32:32 -0600

    added starter code for madlibs, and two tables

---
+++ lib/cli.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sqlite3
+
+conn = sqlite3.connect('project.db')
+cursor = conn.cursor()
+
+title = input("Enter the title of the story: ")
+username = input('Enter your username: ')
+email = input('Enter your email: ')
+story_template = input("Enter the name of the template file: ")
+
+if __name__ == '__main__':
+    with open(story_template, 'r') as file:
+        template = file.read()
+
+words = template.split()
+
+for i in range(len(words)):
+    if words[i].startswith('[') and words[i].endswith(']'):
+        part_of_speech = words[i][1:-1]
+        user_input = input(f"Enter a {part_of_speech}: ")
+        words[i] = user_input
+
+completed_story = ' '.join(words)
+
+print(completed_story)
+
+cursor.execute("INSERT INTO users(username, email) VALUES (?, ?)",
+                (username, email))
+
+cursor.execute("INSERT INTO completed_stories(title, story_template, completed_story) VALUES (?, ?, ?)",
+                (title, story_template, completed_story))
+conn.commit()
