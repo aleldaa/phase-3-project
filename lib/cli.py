2137b62f1eb334b50a7398d5791eb223c5bc82d7
Luna Daniels <aleldaproductions@gmail.com>
2023-05-17 11:23:47 -0600

    updated code to SQLAlchemy

--- lib/cli.py
+++ lib/cli.py
@@ -1,36 +1,54 @@
-import sqlite3
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import declarative_base, relationship, sessionmaker
 
-conn = sqlite3.connect('project.db')
-cursor = conn.cursor()
+# Create the database engine
+engine = create_engine('sqlite:///project.db')
+Base = declarative_base()
 
-cursor.execute("CREATE TABLE IF NOT EXISTS users ("
-               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "username TEXT,"
-               "email TEXT"
-               ")")
+# Define the User model
+class User(Base):
+    __tablename__ = 'users'
 
-cursor.execute("CREATE TABLE IF NOT EXISTS words ("
-               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "word_type TEXT,"
-               "word_text TEXT,"
-               "user_id INTEGER,"
-               "FOREIGN KEY (user_id) REFERENCES users(id)"
-               ")")
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    email = Column(String)
+    words = relationship('Word', back_populates='user')
 
-cursor.execute("CREATE TABLE IF NOT EXISTS completed_stories ("
-               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
-               "title TEXT,"
-               "story_template TEXT,"
-               "completed_story TEXT,"
-               "word_id INTEGER,"
-               "user_id INTEGER,"
-               "FOREIGN KEY (word_id) REFERENCES words(id),"
-               "FOREIGN KEY (user_id) REFERENCES users(id)"
-               ")")
+# Define the Word model
+class Word(Base):
+    __tablename__ = 'words'
 
+    id = Column(Integer, primary_key=True)
+    word_type = Column(String)
+    word_text = Column(String)
+    user_id = Column(Integer, ForeignKey('users.id'))
+    user = relationship('User', back_populates='words')
+    completed_stories = relationship('CompletedStory', back_populates='word')
+
+# Define the CompletedStory model
+class CompletedStory(Base):
+    __tablename__ = 'completed_stories'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String)
+    story_template = Column(String)
+    completed_story = Column(String)
+    word_id = Column(Integer, ForeignKey('words.id'))
+    user_id = Column(Integer, ForeignKey('users.id'))
+    word = relationship('Word', back_populates='completed_stories')
+    user = relationship('User', backref='completed_stories')
+
+# Create the database tables
+Base.metadata.create_all(engine)
+
+# Create a session
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# Prompt user input
 title = input("Enter the title of the story: ")
-username = input('Enter your username: ')
-email = input('Enter your email: ')
+username = input("Enter your username: ")
+email = input("Enter your email: ")
 story_template = input("Enter the name of the template file: ")
 
 if __name__ == '__main__':
@@ -39,25 +57,44 @@ if __name__ == '__main__':
 
 words = template.split()
 
-cursor.execute("INSERT INTO users(username, email) VALUES (?, ?)",
-               (username, email))
-user_id = cursor.lastrowid
+# Create a new user
+user = User(username=username, email=email)
+session.add(user)
+session.commit()
 
-for i in range(len(words)):
-    if words[i].startswith('[') and words[i].endswith(']'):
-        part_of_speech = words[i][1:-1]
+word_objs = []  # List to store Word objects
+
+# Create word objects and associate them with the user
+for i, word in enumerate(words):
+    if word.startswith("[") and word.endswith("]"):
+        part_of_speech = word[1:-1]
         user_input = input(f"Enter a {part_of_speech}: ")
+        word_obj = Word(word_type=part_of_speech, word_text=user_input, user=user)
+        word_objs.append(word_obj)  # Add word_obj to the list
+
+        # Update the word in the template
         words[i] = user_input
-        cursor.execute("INSERT INTO words(word_type, word_text, user_id) VALUES (?, ?, ?)",
-                       (part_of_speech, user_input, user_id))
-        word_id = cursor.lastrowid
-        cursor.execute("UPDATE words SET user_id = ? WHERE id = ?", (user_id, word_id))
 
-completed_story = ' '.join(words)
+# Commit the word objects to the session
+session.add_all(word_objs)
+session.commit()
 
-print(completed_story)
+# Update the completed story with the replaced words
+completed_story = " ".join(words)
 
-cursor.execute("INSERT INTO completed_stories(title, story_template, completed_story, word_id, user_id) VALUES (?, ?, ?, ?, ?)",
-               (title, story_template, completed_story, word_id, user_id))
+# Create a completed story
+completed_story_obj = CompletedStory(
+    title=title,
+    story_template=story_template,
+    completed_story=completed_story,
+    word=word_objs[0],  # Assuming the first word is used in the story
+    user=user
+)
+session.add(completed_story_obj)
+session.commit()
 
-conn.commit()
+# Print the completed story
+print(completed_story_obj.completed_story)
+
+# Close the session
+session.close()
