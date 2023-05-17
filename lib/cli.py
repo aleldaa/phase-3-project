6298558dea942ada7daeda1b6ef8261c48a7cc8e
Luna Daniels <aleldaproductions@gmail.com>
2023-05-17 10:34:11 -0600

    updated code, users & words table now connected

--- lib/cli.py
+++ lib/cli.py
@@ -1,9 +1,35 @@
-#!/usr/bin/env python3
-
 import sqlite3
 
 conn = sqlite3.connect('project.db')
 cursor = conn.cursor()
+
+# Create 'users' table
+cursor.execute("CREATE TABLE IF NOT EXISTS users ("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "username TEXT,"
+               "email TEXT"
+               ")")
+
+# Create 'words' table
+cursor.execute("CREATE TABLE IF NOT EXISTS words ("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "word_type TEXT,"
+               "word_text TEXT,"
+               "user_id INTEGER,"
+               "FOREIGN KEY (user_id) REFERENCES users(id)"
+               ")")
+
+# Create 'completed_stories' table
+cursor.execute("CREATE TABLE IF NOT EXISTS completed_stories ("
+               "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+               "title TEXT,"
+               "story_template TEXT,"
+               "completed_story TEXT,"
+               "word_id INTEGER,"
+               "user_id INTEGER,"
+               "FOREIGN KEY (word_id) REFERENCES words(id),"
+               "FOREIGN KEY (user_id) REFERENCES users(id)"
+               ")")
 
 title = input("Enter the title of the story: ")
 username = input('Enter your username: ')
@@ -16,21 +42,25 @@ if __name__ == '__main__':
 
 words = template.split()
 
+cursor.execute("INSERT INTO users(username, email) VALUES (?, ?)",
+               (username, email))
+user_id = cursor.lastrowid
+
 for i in range(len(words)):
     if words[i].startswith('[') and words[i].endswith(']'):
         part_of_speech = words[i][1:-1]
         user_input = input(f"Enter a {part_of_speech}: ")
         words[i] = user_input
-        cursor.execute("INSERT INTO words(word_type, word_text) VALUES (?, ?)",
-                        (part_of_speech, user_input))
+        cursor.execute("INSERT INTO words(word_type, word_text, user_id) VALUES (?, ?, ?)",
+                       (part_of_speech, user_input, user_id))
+        word_id = cursor.lastrowid
+        cursor.execute("UPDATE words SET user_id = ? WHERE id = ?", (user_id, word_id))
 
 completed_story = ' '.join(words)
 
 print(completed_story)
 
-cursor.execute("INSERT INTO users(username, email) VALUES (?, ?)",
-                (username, email))
+cursor.execute("INSERT INTO completed_stories(title, story_template, completed_story, word_id, user_id) VALUES (?, ?, ?, ?, ?)",
+               (title, story_template, completed_story, word_id, user_id))
 
-cursor.execute("INSERT INTO completed_stories(title, story_template, completed_story) VALUES (?, ?, ?)",
-                (title, story_template, completed_story))
 conn.commit()
